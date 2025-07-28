0e13b53a921f61fead9f274067665cd8b8deee86
sahaj singh <sahajabhin@gmail.com>
2025-07-28 07:44:55 +0530

    App router

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main><div>hello world</div></main>
+    <main>
+      <div>
+        <Link href={'/users'}>users</Link>
+      </div>
+    </main>
   );
 }
